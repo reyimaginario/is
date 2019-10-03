74d6028295ae644236bbebe7be9aaf5c53de6053
Charly <charly021@gmail.com>
2019-10-02 21:06:30 -0300

    estructura inicial

---
+++ backend/athos/src/main/java/com/hexsoft/athos/AthosApplication.java
@@ -0,0 +1,13 @@
+package com.hexsoft.athos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthosApplication.class, args);
+	}
+
+}
