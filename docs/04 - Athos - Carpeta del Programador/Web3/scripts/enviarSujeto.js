14d70f982a3645d4624b912c4513c4c721a96f1d
Ninja <mediavilla.agustin@gmail.com>
2019-11-09 16:39:37 -0300

    pantallas listas

--- docs/04 - Athos - Carpeta del Programador/Web3/scripts/enviarSujeto.js
+++ docs/04 - Athos - Carpeta del Programador/Web3/scripts/enviarSujeto.js
@@ -1,11 +1,36 @@
 var formulario = document.getElementById("formulario");
-var url = "http://localhost:3000/chat/user/login/";
+var url = "http://192.168.0.127:8080/evaluacion";
 
 formulario.addEventListener("submit", function(e){
     e.preventDefault()
 
+    var evaluacion = createJson()
+    
+    fetch(url, {
+        method: 'POST',
+        //mode: 'no-cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(evaluacion) 
+    })
+
+    .then( res => res.json())
+    .then( data => {
+        console.log(data)
+        //sessionStorage.setItem("evaluacion", data)
+        //ACA GUARDAR LA RESPUESTA EN EL LOCAL STORAGE DEL BROWSER
+        //JSON CON PREGUNTAS, JSON CON EVALUACION, CREAR EL INDICE PARA RECORRER LAS PREGUNTAS
+        //cargar nueva pagina
+    })
+    .catch(err => {
+        console.error('Caught error: ', err)
+        alert("Ocurrio un error! :c")
+    });
+});
+
+function createJson(){
     var datos = new FormData(formulario);
-    var booty = {
+
+    var evaluacion = {
         evaluacionId: null,
         fechaInicio: "",
         fechaFin: "",
@@ -36,21 +61,11 @@ formulario.addEventListener("submit", function(e){
         },
         listaTestsAplicadosDTO: [{
             testCode:"mmpi2",
+            listaPreguntas:[],
             listaRespuestasDTO:[]
         }],
         informe: ""
     };
-    
-    fetch(url, {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(booty) 
-    })
 
-    .then( res => res.json())
-    .then( data => {
-        console.log(data)
-    })
-    .catch(err => console.error('Caught error: ', err))
-});
+    return evaluacion
+}
