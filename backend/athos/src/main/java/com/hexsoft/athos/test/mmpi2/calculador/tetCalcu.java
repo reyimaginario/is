bd303aa1a3773cc9cc62407433c35c4fc74e935a
SantoGM <suarez_s@hotmail.com>
2019-11-16 13:02:49 -0300

    Cambios calcu

--- backend/athos/src/main/java/com/hexsoft/athos/test/mmpi2/calculador/tetCalcu.java
+++ backend/athos/src/main/java/com/hexsoft/athos/test/mmpi2/calculador/tetCalcu.java
@@ -3,6 +3,7 @@ package com.hexsoft.athos.test.mmpi2.calculador;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Iterator;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -13,7 +14,11 @@ public class tetCalcu {
 
 	public static void main(String[] args) throws Exception {			
 		JSONObject respuestasJSON = (JSONObject) readJsonSimpleDemo("src\\main\\java\\com\\hexsoft\\athos\\test\\mmpi2\\calculador\\respuestas.json");
-		System.out.println(CalculadorEscalasAdicionalesDeValidez.getInstance().construirEscalas(respuestasJSON));
+		JSONObject respuestasTest = CalculadorEscalasAdicionalesDeValidez.getInstance().construirEscalas(respuestasJSON);
+		Iterator it = respuestasTest.entrySet().iterator();
+		while (it.hasNext()) {			
+			System.out.println(it.next());
+		}
 		/*File f = new File("C:\\Users\\suare\\Desktop\\Coso loco.txt");
 		BufferedReader r = new BufferedReader(new FileReader(f));
 		while (r.ready()) {
