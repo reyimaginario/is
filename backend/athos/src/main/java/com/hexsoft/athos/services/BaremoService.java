2c528d5ca76771228f58663e87112f08d733019a
Gonzalo Caba <charly021@gmail.com>
2019-11-30 19:32:45 -0300

    changes

--- backend/athos/src/main/java/com/hexsoft/athos/services/BaremoService.java
+++ backend/athos/src/main/java/com/hexsoft/athos/services/BaremoService.java
@@ -1,5 +1,6 @@
 package com.hexsoft.athos.services;
 
+import com.hexsoft.athos.entities.BaremoDAO;
 import com.hexsoft.athos.entities.SujetoAnonimo;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,22 @@ import java.util.List;
 public class BaremoService {
 
     public void guardarBaremo(SujetoAnonimo sujetoAnonimo, List<JSONObject> listaResultados) {
+
+        JSONObject mmpi2 = listaResultados.get(0);  // primer test mmpi2
+
+        JSONObject escalaBasica = (JSONObject)mmpi2.get("Basicas");
+
+        BaremoDAO baremoDAO = new BaremoDAO();
+
+        // sujeto anonimo
+        baremoDAO.setLocalidad(sujetoAnonimo.getLocalidad());
+        baremoDAO.setEdad(sujetoAnonimo.getEdad());
+        baremoDAO.setEdad(sujetoAnonimo.getGenero());
+        baremoDAO.setNivelDeEstudio(sujetoAnonimo.getNivelDeEstudio());
+        baremoDAO.setOcupacion(sujetoAnonimo.getOcupacion());
+
+        // escala basica
+        //baremoDAO.setBasica_l();
 
     }
 }
