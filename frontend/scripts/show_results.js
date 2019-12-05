1e52952fe255bf10735b9b2cd0f29f650a791ae1
SantoGM <suarez_s@hotmail.com>
2019-12-05 08:13:44 -0300

    Pantalla y scripts de Baremo

--- frontend/scripts/show_results.js
+++ frontend/scripts/show_results.js
@@ -1,423 +1,437 @@
-var jason ={
-    "Contenido": [
-      {
-        "Escala": "SUBESCALA_ANX",
-        "PB": 9,
-        "PTF": 55,
-        "PTM": 57
-      },
-      {
-        "Escala": "SUBESCALA_FRS",
-        "PB": 15,
-        "PTF": 75,
-        "PTM": 87
-      },
-      {
-        "Escala": "SUBESCALA_OBS",
-        "PB": 6,
-        "PTF": 50,
-        "PTM": 53
-      },
-      {
-        "Escala": "SUBESCALA_DEP",
-        "PB": 3,
-        "PTF": 45,
-        "PTM": 48
-      },
-      {
-        "Escala": "SUBESCALA_HEA",
-        "PB": 2,
-        "PTF": 40,
-        "PTM": 41
-      },
-      {
-        "Escala": "SUBESCALA_BIZ",
-        "PB": 1,
-        "PTF": 47,
-        "PTM": 46
-      },
-      {
-        "Escala": "SUBESCALA_ANG",
-        "PB": 7,
-        "PTF": 53,
-        "PTM": 53
-      },
-      {
-        "Escala": "SUBESCALA_CYN",
-        "PB": 4,
-        "PTF": 42,
-        "PTM": 41
-      },
-      {
-        "Escala": "SUBESCALA_ASP",
-        "PB": 5,
-        "PTF": 47,
-        "PTM": 44
-      },
-      {
-        "Escala": "SUBESCALA_TPA",
-        "PB": 12,
-        "PTF": 64,
-        "PTM": 60
-      },
-      {
-        "Escala": "SUBESCALA_LSE",
-        "PB": 4,
-        "PTF": 49,
-        "PTM": 51
-      },
-      {
-        "Escala": "SUBESCALA_SOD",
-        "PB": 7,
-        "PTF": 49,
-        "PTM": 49
-      },
-      {
-        "Escala": "SUBESCALA_FAM",
-        "PB": 4,
-        "PTF": 45,
-        "PTM": 47
-      },
-      {
-        "Escala": "SUBESCALA_WRK",
-        "PB": 4,
-        "PTF": 43,
-        "PTM": 44
-      },
-      {
-        "Escala": "SUBESCALA_TRT",
-        "PB": 0,
-        "PTF": 35,
-        "PTM": 35
-      }
-    ],
-    "Suplementarias": [
-      {
-        "Escala": "SUBESCALA_A",
-        "PB": 7,
-        "PTF": 44,
-        "PTM": 46
-      },
-      {
-        "Escala": "SUBESCALA_R",
-        "PB": 19,
-        "PTF": 57,
-        "PTM": 58
-      },
-      {
-        "Escala": "SUBESCALA_ES",
-        "PB": 33,
-        "PTF": 47,
-        "PTM": 40
-      },
-      {
-        "Escala": "SUBESCALA_MAC_R",
-        "PB": 23,
-        "PTF": 61,
-        "PTM": 55
-      },
-      {
-        "Escala": "SUBESCALA_O_H",
-        "PB": 18,
-        "PTF": 66,
-        "PTM": 69
-      },
-      {
-        "Escala": "SUBESCALA_DO",
-        "PB": 17,
-        "PTF": 53,
-        "PTM": 51
-      },
-      {
-        "Escala": "SUBESCALA_RE",
-        "PB": 20,
-        "PTF": 47,
-        "PTM": 50
-      },
-      {
-        "Escala": "SUBESCALA_MT",
-        "PB": 14,
-        "PTF": 52,
-        "PTM": 54
-      },
-      {
-        "Escala": "SUBESCALA_GM",
-        "PB": 30,
-        "PTF": 52,
-        "PTM": 34
-      },
-      {
-        "Escala": "SUBESCALA_GF",
-        "PB": 34,
-        "PTF": 43,
-        "PTM": 64
-      },
-      {
-        "Escala": "SUBESCALA_PK",
-        "PB": 7,
-        "PTF": 48,
-        "PTM": 48
-      },
-      {
-        "Escala": "SUBESCALA_PS",
-        "PB": 5,
-        "PTF": 42,
-        "PTM": 43
-      },
-      {
-        "Escala": "SUBESCALA_SI1",
-        "PB": 5,
-        "PTF": 49,
-        "PTM": 51
-      },
-      {
-        "Escala": "SUBESCALA_SI2",
-        "PB": 3,
-        "PTF": 51,
-        "PTM": 49
-      },
-      {
-        "Escala": "SUBESCALA_SI3",
-        "PB": 6,
-        "PTF": 52,
-        "PTM": 53
-      },
-      {
-        "Escala": "SUBESCALA_MDS",
-        "PB": 2,
-        "PTF": 49,
-        "PTM": 49
-      },
-      {
-        "Escala": "SUBESCALA_APS",
-        "PB": 20,
-        "PTF": 42,
-        "PTM": 41
-      },
-      {
-        "Escala": "SUBESCALA_AAS",
-        "PB": 0,
-        "PTF": 39,
-        "PTM": 36
-      }
-    ],
-    "Basicas": [
-      {
-        "Escala": "SUBESCALA_L",
-        "PB": 7,
-        "PTF": 66,
-        "PTM": 65
-      },
-      {
-        "Escala": "SUBESCALA_F",
-        "PB": 2,
-        "PTF": 44,
-        "PTM": 42
-      },
-      {
-        "Escala": "SUBESCALA_K",
-        "PB": 19,
-        "PTF": 59,
-        "PTM": 58
-      },
-      {
-        "Escala": "SUBESCALA_HS",
-        "PB": 11,
-        "PTF": 43,
-        "PTM": 45
-      },
-      {
-        "Escala": "SUBESCALA_D",
-        "PB": 18,
-        "PTF": 46,
-        "PTM": 50
-      },
-      {
-        "Escala": "SUBESCALA_HY",
-        "PB": 17,
-        "PTF": 39,
-        "PTM": 42
-      },
-      {
-        "Escala": "SUBESCALA_PD",
-        "PB": 19,
-        "PTF": 43,
-        "PTM": 42
-      },
-      {
-        "Escala": "SUBESCALA_MF_M",
-        "PB": 24,
-        "PTF": null,
-        "PTM": 46
-      },
-      {
-        "Escala": "SUBESCALA_MF_F",
-        "PB": 28,
-        "PTF": 69,
-        "PTM": null
-      },
-      {
-        "Escala": "SUBESCALA_PA",
-        "PB": 9,
-        "PTF": 45,
-        "PTM": 46
-      },
-      {
-        "Escala": "SUBESCALA_PT",
-        "PB": 29,
-        "PTF": 53,
-        "PTM": 55
-      },
-      {
-        "Escala": "SUBESCALA_SC",
-        "PB": 23,
-        "PTF": 44,
-        "PTM": 44
-      },
-      {
-        "Escala": "SUBESCALA_MA",
-        "PB": 16,
-        "PTF": 43,
-        "PTM": 41
-      },
-      {
-        "Escala": "SUBESCALA_SI",
-        "PB": 32,
-        "PTF": 54,
-        "PTM": 57
-      }
-    ],
-    "Adicionales de Validez": [
-      {
-        "Escala": "SUBESCALA_FB_BACK",
-        "PB": 2,
-        "PTF": 50,
-        "PTM": 51
-      },
-      {
-        "Escala": "SUBESCALA_F(P)",
-        "PB": 3,
-        "PTF": 65,
-        "PTM": 63
-      },
-      {
-        "Escala": "SUBESCALA_DS",
-        "PB": 10,
-        "PTF": null,
-        "PTM": null
-      },
-      {
-        "Escala": "SUBESCALA_DS_R",
-        "PB": 6,
-        "PTF": null,
-        "PTM": null
-      },
-      {
-        "Escala": "SUBESCALA_S",
-        "PB": 30,
-        "PTF": null,
-        "PTM": null
-      },
-      {
-        "Escala": "SUBESCALA_SD",
-        "PB": 18,
-        "PTF": null,
-        "PTM": null
-      },
-      {
-        "Escala": "SUBESCALA_SO",
-        "PB": 32,
-        "PTF": null,
-        "PTM": null
-      },
-      {
-        "Escala": "SUBESCALA_VRIN",
-        "PB": 7,
-        "PTF": 58,
-        "PTM": 57
-      },
-      {
-        "Escala": "SUBESCALA_TRIN",
-        "PB": 0,
-        "PTF": null,
-        "PTM": null
-      }
-    ]
-  }
-    
-function showResults(){
-    var url = "http://localhost:8080/evaluacion/" + sessionStorage.getItem("evaluacion_id") + "/calcular"
-    fetch(url, {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'},
-     })
-    .then( res => res.json())
-    .then( data => {
-        console.log(data)
-        saveResponse("resultados", JSON.stringify(data))
+var jason = {
+  Contenido: [
+    {
+      Escala: "SUBESCALA_ANX",
+      PB: 9,
+      PTF: 55,
+      PTM: 57
+    },
+    {
+      Escala: "SUBESCALA_FRS",
+      PB: 15,
+      PTF: 75,
+      PTM: 87
+    },
+    {
+      Escala: "SUBESCALA_OBS",
+      PB: 6,
+      PTF: 50,
+      PTM: 53
+    },
+    {
+      Escala: "SUBESCALA_DEP",
+      PB: 3,
+      PTF: 45,
+      PTM: 48
+    },
+    {
+      Escala: "SUBESCALA_HEA",
+      PB: 2,
+      PTF: 40,
+      PTM: 41
+    },
+    {
+      Escala: "SUBESCALA_BIZ",
+      PB: 1,
+      PTF: 47,
+      PTM: 46
+    },
+    {
+      Escala: "SUBESCALA_ANG",
+      PB: 7,
+      PTF: 53,
+      PTM: 53
+    },
+    {
+      Escala: "SUBESCALA_CYN",
+      PB: 4,
+      PTF: 42,
+      PTM: 41
+    },
+    {
+      Escala: "SUBESCALA_ASP",
+      PB: 5,
+      PTF: 47,
+      PTM: 44
+    },
+    {
+      Escala: "SUBESCALA_TPA",
+      PB: 12,
+      PTF: 64,
+      PTM: 60
+    },
+    {
+      Escala: "SUBESCALA_LSE",
+      PB: 4,
+      PTF: 49,
+      PTM: 51
+    },
+    {
+      Escala: "SUBESCALA_SOD",
+      PB: 7,
+      PTF: 49,
+      PTM: 49
+    },
+    {
+      Escala: "SUBESCALA_FAM",
+      PB: 4,
+      PTF: 45,
+      PTM: 47
+    },
+    {
+      Escala: "SUBESCALA_WRK",
+      PB: 4,
+      PTF: 43,
+      PTM: 44
+    },
+    {
+      Escala: "SUBESCALA_TRT",
+      PB: 0,
+      PTF: 35,
+      PTM: 35
+    }
+  ],
+  Suplementarias: [
+    {
+      Escala: "SUBESCALA_A",
+      PB: 7,
+      PTF: 44,
+      PTM: 46
+    },
+    {
+      Escala: "SUBESCALA_R",
+      PB: 19,
+      PTF: 57,
+      PTM: 58
+    },
+    {
+      Escala: "SUBESCALA_ES",
+      PB: 33,
+      PTF: 47,
+      PTM: 40
+    },
+    {
+      Escala: "SUBESCALA_MAC_R",
+      PB: 23,
+      PTF: 61,
+      PTM: 55
+    },
+    {
+      Escala: "SUBESCALA_O_H",
+      PB: 18,
+      PTF: 66,
+      PTM: 69
+    },
+    {
+      Escala: "SUBESCALA_DO",
+      PB: 17,
+      PTF: 53,
+      PTM: 51
+    },
+    {
+      Escala: "SUBESCALA_RE",
+      PB: 20,
+      PTF: 47,
+      PTM: 50
+    },
+    {
+      Escala: "SUBESCALA_MT",
+      PB: 14,
+      PTF: 52,
+      PTM: 54
+    },
+    {
+      Escala: "SUBESCALA_GM",
+      PB: 30,
+      PTF: 52,
+      PTM: 34
+    },
+    {
+      Escala: "SUBESCALA_GF",
+      PB: 34,
+      PTF: 43,
+      PTM: 64
+    },
+    {
+      Escala: "SUBESCALA_PK",
+      PB: 7,
+      PTF: 48,
+      PTM: 48
+    },
+    {
+      Escala: "SUBESCALA_PS",
+      PB: 5,
+      PTF: 42,
+      PTM: 43
+    },
+    {
+      Escala: "SUBESCALA_SI1",
+      PB: 5,
+      PTF: 49,
+      PTM: 51
+    },
+    {
+      Escala: "SUBESCALA_SI2",
+      PB: 3,
+      PTF: 51,
+      PTM: 49
+    },
+    {
+      Escala: "SUBESCALA_SI3",
+      PB: 6,
+      PTF: 52,
+      PTM: 53
+    },
+    {
+      Escala: "SUBESCALA_MDS",
+      PB: 2,
+      PTF: 49,
+      PTM: 49
+    },
+    {
+      Escala: "SUBESCALA_APS",
+      PB: 20,
+      PTF: 42,
+      PTM: 41
+    },
+    {
+      Escala: "SUBESCALA_AAS",
+      PB: 0,
+      PTF: 39,
+      PTM: 36
+    }
+  ],
+  Basicas: [
+    {
+      Escala: "SUBESCALA_L",
+      PB: 7,
+      PTF: 66,
+      PTM: 65
+    },
+    {
+      Escala: "SUBESCALA_F",
+      PB: 2,
+      PTF: 44,
+      PTM: 42
+    },
+    {
+      Escala: "SUBESCALA_K",
+      PB: 19,
+      PTF: 59,
+      PTM: 58
+    },
+    {
+      Escala: "SUBESCALA_HS",
+      PB: 11,
+      PTF: 43,
+      PTM: 45
+    },
+    {
+      Escala: "SUBESCALA_D",
+      PB: 18,
+      PTF: 46,
+      PTM: 50
+    },
+    {
+      Escala: "SUBESCALA_HY",
+      PB: 17,
+      PTF: 39,
+      PTM: 42
+    },
+    {
+      Escala: "SUBESCALA_PD",
+      PB: 19,
+      PTF: 43,
+      PTM: 42
+    },
+    {
+      Escala: "SUBESCALA_MF_M",
+      PB: 24,
+      PTF: null,
+      PTM: 46
+    },
+    {
+      Escala: "SUBESCALA_MF_F",
+      PB: 28,
+      PTF: 69,
+      PTM: null
+    },
+    {
+      Escala: "SUBESCALA_PA",
+      PB: 9,
+      PTF: 45,
+      PTM: 46
+    },
+    {
+      Escala: "SUBESCALA_PT",
+      PB: 29,
+      PTF: 53,
+      PTM: 55
+    },
+    {
+      Escala: "SUBESCALA_SC",
+      PB: 23,
+      PTF: 44,
+      PTM: 44
+    },
+    {
+      Escala: "SUBESCALA_MA",
+      PB: 16,
+      PTF: 43,
+      PTM: 41
+    },
+    {
+      Escala: "SUBESCALA_SI",
+      PB: 32,
+      PTF: 54,
+      PTM: 57
+    }
+  ],
+  "Adicionales de Validez": [
+    {
+      Escala: "SUBESCALA_FB_BACK",
+      PB: 2,
+      PTF: 50,
+      PTM: 51
+    },
+    {
+      Escala: "SUBESCALA_F(P)",
+      PB: 3,
+      PTF: 65,
+      PTM: 63
+    },
+    {
+      Escala: "SUBESCALA_DS",
+      PB: 10,
+      PTF: null,
+      PTM: null
+    },
+    {
+      Escala: "SUBESCALA_DS_R",
+      PB: 6,
+      PTF: null,
+      PTM: null
+    },
+    {
+      Escala: "SUBESCALA_S",
+      PB: 30,
+      PTF: null,
+      PTM: null
+    },
+    {
+      Escala: "SUBESCALA_SD",
+      PB: 18,
+      PTF: null,
+      PTM: null
+    },
+    {
+      Escala: "SUBESCALA_SO",
+      PB: 32,
+      PTF: null,
+      PTM: null
+    },
+    {
+      Escala: "SUBESCALA_VRIN",
+      PB: 7,
+      PTF: 58,
+      PTM: 57
+    },
+    {
+      Escala: "SUBESCALA_TRIN",
+      PB: 0,
+      PTF: null,
+      PTM: null
+    }
+  ]
+};
+
+function showResults() {
+  var url =
+    "http://localhost:8080/evaluacion/" +
+    sessionStorage.getItem("evaluacion_id") +
+    "/calcular";
+  fetch(url, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      saveResponse("resultados", JSON.stringify(data));
     })
     .catch(err => {
-        console.error('Caught error: ', err)
-        alert("Ocurrio un error en GET! :c")
+      console.error("Caught error: ", err);
+      alert("Ocurrio un error en GET! :c");
     });
 }
 
-var titulos = ["Basicas", "Contenido", "Suplementarias", "Adicionales de Validez"]
+var titulos = [
+  "Basicas",
+  "Contenido",
+  "Suplementarias",
+  "Adicionales de Validez"
+];
 
-async function crearLista(){
-    await showResults()
+async function crearLista() {
+  await showResults();
 
-
-
-    jason = JSON.parse(sessionStorage.getItem("resultados"))
-    console.log(jason)
-    for (j = 0; j < titulos.length; j++) {
-        crearTabla(titulos[j])
-        console.log(j)
-    }
-
+  jason = JSON.parse(sessionStorage.getItem("resultados"));
+  console.log(jason);
+  for (j = 0; j < titulos.length; j++) {
+    crearTabla(titulos[j]);
+    console.log(j);
+  }
 }
 
 function crearTabla(titulo) {
-    var lista = jason[0][titulo]
-    console.log(lista)
-    var tabla = document.getElementById("lista_preguntas"); 
-    
-    var header = document.createElement("thead");
-    //header.id = titulo;
-    header.className = "form-group";
-    tabla.appendChild(header);
-    header.innerHTML = titulo;
+  var lista = jason[0][titulo];
+  console.log(lista);
+  var tabla = document.getElementById("lista_preguntas");
+  tabla.className = "result-table";
+  var header = document.createElement("th");
+  tabla.append(header);
+  header.colSpan = 4;
+  header.innerHTML = titulo;
 
-    var body = document.createElement("tbody");
-    //body.id = titulo;
-    body.className = "form-group";
-    tabla.appendChild(body);
-    body.innerHTML = "";
+  var body = document.createElement("tbody");
+  //body.id = titulo;
+  body.className = "form-group";
+  tabla.appendChild(body);
+  body.innerHTML = "";
 
-    var topRow = document.createElement("tr");
-    topRow.className = "form-group";
-    body.appendChild(topRow);
-    topRow.innerHTML = "<td><a>Escala</a></td>" + 
-                        "<td><a>PB</a></td>" +
-                        "<td><a>PTF</a></td>" +
-                        "<td><a>PTM</a></td>";
+  var topRow = document.createElement("tr");
+  topRow.className = "form-group";
+  body.appendChild(topRow);
+  topRow.innerHTML =
+    "<td ><h4>Escala</h4></td>" +
+    "<td align='center'><h4>PB</h4></td>" +
+    "<td align='center'><h4>PTF</h4></td>" +
+    "<td align='center'><h4>PTM</h4></td>";
 
-    for (i = 0; i < lista.length; i++) {
-        var container = document.createElement("tr");
-        container.className = "form-group";
-        body.appendChild(container);
-        container.innerHTML = "<td><a>" + lista[i].Escala.replace("SUBESCALA_", "") + "</a></td>" + 
-                              "<td><a>" + lista[i].PB + "</a></td>" +
-                              "<td><a>" + lista[i].PTF + "</a></td>" +
-                              "<td><a>" + lista[i].PTM + "</a></td>";
-    }
+  for (i = 0; i < lista.length; i++) {
+    var container = document.createElement("tr");
+    container.className = "form-group";
+    body.appendChild(container);
+    container.innerHTML =
+      "<td><a>" +
+      lista[i].Escala.replace("SUBESCALA_", "") +
+      "</a></td>" +
+      "<td align='center'><a>" +
+      lista[i].PB +
+      "</a></td>" +
+      "<td align='center'><a>" +
+      (lista[i].PTF != null ? lista[i].PTF : "-") +
+      "</a></td>" +
+      "<td align='center'><a>" +
+      (lista[i].PTM != null ? lista[i].PTM : "-") +
+      "</a></td>";
+  }
 }
 
-function saveResponse(name, data){
-    if (typeof(Storage) !== 'undefined') {
-        sessionStorage.setItem(name,data)
-    } else {
-        alert("No hay Local Storage! :c")
-    }
+function saveResponse(name, data) {
+  if (typeof Storage !== "undefined") {
+    sessionStorage.setItem(name, data);
+  } else {
+    alert("No hay Local Storage! :c");
+  }
 }
